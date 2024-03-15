eddbf5bea8d227e4a41a3e08d5ad9f482fcb0211
Gabriel Tavares <gabrielgstell@gmail.com>
2024-03-15 14:53:37 -0300

    Exercício 10  finalizado

--- Exercicio_10.ConsoleApp/Program.cs
+++ Exercicio_10.ConsoleApp/Program.cs
@@ -2,9 +2,32 @@
 {
     internal class Program
     {
+        //Crie um programa para calcular a média ponderada de duas provas realizadas por um aluno
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double peso1, peso2 = default;
+            double nota1, nota2 = default;
+            double mediaPonderada = default;
+
+
+            Console.WriteLine("Calculando Media Ponderada!\n");
+
+            Console.WriteLine("Informe a primeira nota: ");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nInforme o peso da primeira nota: ");
+            peso1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nInforme a segunda nota: ");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nInforme o peso da segunda nota: ");
+            peso2 = Convert.ToDouble(Console.ReadLine());
+
+            //calculo da media ponderada
+            mediaPonderada = ((nota1 * peso1) + (nota2 * peso2)) / (peso1 + peso2);
+
+            Console.WriteLine($"\nA média ponderada é: {mediaPonderada}");
         }
     }
 }
