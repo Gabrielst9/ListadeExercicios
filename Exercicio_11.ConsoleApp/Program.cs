ac5569c49dae160c025b614176374f7eac7748d0
Gabriel Tavares <gabrielgstell@gmail.com>
2024-03-15 15:34:41 -0300

    Adicionando os exercicios 11 e 12

---
+++ Exercicio_11.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercicio_11.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Removido :(");
+        }
+    }
+}
---
+++ Exercicio_12.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercicio_12.ConsoleApp
+{
+    internal class Program
+    {
+        //A imobiliária Imóbilis vende apenas terrenos retangulares.
+        //Faça um algoritmo para ler as dimensões de um terreno e depois exibir a área do terreno.
+        static void Main(string[] args)
+        {
+            double largura, comprimento = default;
+            double area = default;
+
+            Console.WriteLine("Calculo da Area do Terreno");
+
+            Console.WriteLine("Informe a largura do terreno: ");
+            largura = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nInforme o comprimento do terreno: ");
+            comprimento = Convert.ToDouble(Console.ReadLine());
+
+            //calculo da area
+            area = largura * comprimento;
+
+            Console.WriteLine($"\nA área do terreno é: {area}");
+        }
+    }
+}
