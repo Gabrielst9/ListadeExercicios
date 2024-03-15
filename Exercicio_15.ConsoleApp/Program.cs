00c6375fe265a891345aa8966e41ae640700bae9
Gabriel Tavares <gabrielgstell@gmail.com>
2024-03-15 16:11:45 -0300

    Exercícios do 11 até o 16 completos.

---
+++ Exercicio_15.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercicio_15.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("removido :(");
+        }
+    }
+}
---
+++ Exercicio_16.ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicio_16.ConsoleApp
+{
+    internal class Program
+    {
+        //Faça um algoritmo para ler o salário de um funcionário e aumentá-lo em 15%.
+        //Após o aumento, desconte 8% de impostos.
+        //Imprima o salário inicial, o salário com o aumento e o salário final.
+        static void Main(string[] args)
+        {
+            decimal salario = default;
+            decimal aumento = default;
+            decimal imposto = 0.08m;
+            decimal salarioFinal = default;
+
+            Console.WriteLine("Calculo de Aumento Salarial");
+
+            Console.WriteLine("Informe o salário do funcionário: ");
+            salario = decimal.Parse(Console.ReadLine());
+
+            //calculo
+            aumento = salario * 0.15m;
+            salarioFinal = salario + aumento - (imposto * salario);
+
+            Console.WriteLine($"\nSalário inicial: {salario}");
+            Console.WriteLine($"Salário com aumento: {salario + aumento}");
+        }
+    }
+}
